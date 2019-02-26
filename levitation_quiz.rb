d03b451b1ca531a513eb2630282707bd6ad0abd9
delightful-tree-6762 <mduckworth@gmail.com>
2019-02-26 06:40:59 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,9 +1,1 @@
 
-def levitation_quiz
-  loops do
-    puts "What is the spell that enacts levitation?"
-    answer = gets.chomp
-    breaks if answer == "Wingardium Leviosa"
-end
-puts "You passed the quiz"
-end
